79a94d66d310a05098e28dccb3c09f92a3fcbbb6
gaurav.jadhav <gaurav.jadhav@octalsoftware.com>
2023-12-15 18:53:56 +0530

    fix

--- src/components/reusable/OInputField.js
+++ src/components/reusable/OInputField.js
@@ -58,6 +58,31 @@ const OInputField = props => {
         </div>
       )
 
+      case 'password':
+        return (
+          <div className={wrapperClassName || 'relative z-0 mb-6 w-full group'}>
+            <label
+              htmlFor={name}
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+            >
+              {inputLabel}
+              {labelType && <span className='text-red-500'>*</span>}
+            </label>
+            <input
+              type={type}
+              name={name}
+              id={name}
+              className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder={placeholder || ' '}
+              maxLength={maxLength}
+              {...register}
+              {...rest}
+              disabled={disable}
+            />
+  
+            <ErrorMessage message={errors?.[name]?.message} />
+          </div>
+        )
     case 'textarea':
       return (
         <div className={wrapperClassName || 'relative z-0 mb-6 w-full group'}>
