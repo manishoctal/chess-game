032e445d8ab9c14cb3f9152203be5fd54607fadf
gaurav.jadhav <gaurav.jadhav@octalsoftware.com>
2023-12-15 13:02:32 +0530

    fix

--- src/components/reusable/OInputField.js
+++ src/components/reusable/OInputField.js
@@ -18,6 +18,32 @@ const OInputField = props => {
     ...rest
   } = props
 
+  const emailPassword =()=>
+    
+    <div className={wrapperClassName || 'relative z-0 mb-6 w-full group'}>
+            <label
+              htmlFor={name}
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+            >
+              {inputLabel}
+              {labelType && <span className='text-red-500'>*</span>}
+            </label>
+            <input
+              type={type}
+              name={name}
+              id={name}
+              className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder={placeholder || ' '}
+              maxLength={maxLength}
+              {...register}
+              {...rest}
+              disabled={disable}
+            />
+
+            <ErrorMessage message={errors?.[name]?.message} />
+          </div>
+  
+
   switch (type) {
     case 'hidden':
       return (
@@ -34,29 +60,7 @@ const OInputField = props => {
     case 'url':
     case 'email':
       return (
-        <div className={wrapperClassName || 'relative z-0 mb-6 w-full group'}>
-          <label
-            htmlFor={name}
-            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-          >
-            {inputLabel}
-            {labelType && <span className='text-red-500'>*</span>}
-          </label>
-
-          <input
-            type={type}
-            name={name}
-            id={name}
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder={placeholder || ' '}
-            maxLength={maxLength}
-            {...register}
-            {...rest}
-            disabled={disable}
-          />
-
-          <ErrorMessage message={errors?.[name]?.message} />
-        </div>
+        emailPassword()
       )
 
     case 'textarea':
@@ -86,30 +90,7 @@ const OInputField = props => {
       )
     case 'password':
       return (
-        <>
-          <div className={wrapperClassName || 'relative z-0 mb-6 w-full group'}>
-            <label
-              htmlFor={name}
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-            >
-              {inputLabel}
-              {labelType && <span className='text-red-500'>*</span>}
-            </label>
-            <input
-              type={type}
-              name={name}
-              id={name}
-              className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-              placeholder={placeholder || ' '}
-              maxLength={maxLength}
-              {...register}
-              {...rest}
-              disabled={disable}
-            />
-
-            <ErrorMessage message={errors?.[name]?.message} />
-          </div>
-        </>
+        emailPassword()
       )
     case 'select':
       return (
