c9e43ff79d63cfd76b94181b470b553cce09c4b6
gaurav.jadhav <gaurav.jadhav@octalsoftware.com>
2023-12-21 12:04:49 +0530

    fix

--- src/pages/reports_manager/Report.js
+++ src/pages/reports_manager/Report.js
@@ -146,41 +146,21 @@ function Report () {
       <div className='bg-[#F9F9F9] dark:bg-slate-900'>
         <div className='px-3 py-4'>
           <div className='flex justify-center items-center grid grid-cols-2 w-[500px]'>
-            <button
-              type='button'
-              title= {t('FOREIGN_TOURIST')}
-              className={`pr-6 bg-white border border-1 border-[#000] text-sm px-8 ml-3 mb-3 py-2 rounded-lg items-center  text-black  sm:w-auto w-1/2 ${userType==='tourist' && 'bg-[#000!important] text-white'}`}
-              onClick={() => {setUserType('tourist');handleReset()}}
+            <button   type='button' title= {t('FOREIGN_TOURIST')}  className={`pr-6 bg-white border border-1 border-[#000] text-sm px-8 ml-3 mb-3 py-2 rounded-lg items-center  text-black  sm:w-auto w-1/2 ${userType==='tourist' && 'bg-[#000!important] text-white'}`} onClick={() => {setUserType('tourist');handleReset()}}
             >
-              {t('FOREIGN_TOURIST')}
+            {t('FOREIGN_TOURIST')}
             </button>
-            <button
-              type='button'
-              title= {t('THAI_LOCAL')}
-              className={` pr-6 bg-white border border-1 border-[#000] text-sm px-8 ml-3 mb-3 py-2 rounded-lg items-center  text-black  sm:w-auto w-1/2 ${userType==='local'&& 'bg-[#000!important] text-white'}`}
-              onClick={() => {setUserType('local');handleReset()}}
+            <button type='button'  title= {t('THAI_LOCAL')} className={` pr-6 bg-white border border-1 border-[#000] text-sm px-8 ml-3 mb-3 py-2 rounded-lg items-center  text-black  sm:w-auto w-1/2 ${userType==='local'&& 'bg-[#000!important] text-white'}`} onClick={() => {setUserType('local');handleReset()}}
             >
-              {t('THAI_LOCAL')}
+            {t('THAI_LOCAL')}
             </button>
           </div>
           <div className='bg-white border border-[#E9EDF9] rounded-lg dark:bg-slate-800 dark:border-[#ffffff38]'>
             <form className='border-b border-b-[#E3E3E3]  px-4 py-3 pt-5 flex flex-wrap justify-between'>
               <div className='flex flex-wrap items-center'>
                 <div className='flex items-center lg:pt-0 pt-3 justify-center'> 
-                  <ODateRangePicker
-                    handleDateChange={handleDateChange}
-                    isReset={filterData?.isReset}
-                    setIsReset={setFilterData}
-                  />
-
-                 
-
-
-                  <button
-                    type='button'
-                    onClick={() => handleReset()}
-                    className='bg-gradientTo text-sm px-8 ml-3 mb-3 py-2 rounded-lg items-center border border-transparent text-white hover:bg-DarkBlue sm:w-auto w-1/2'
-                    title= {t('O_RESET')}
+                  <ODateRangePicker handleDateChange={handleDateChange} isReset={filterData?.isReset} setIsReset={setFilterData}/>
+                  <button type='button' onClick={() => handleReset()}  className='bg-gradientTo text-sm px-8 ml-3 mb-3 py-2 rounded-lg items-center border border-transparent text-white hover:bg-DarkBlue sm:w-auto w-1/2' title= {t('O_RESET')}
                   >
                     {t('O_RESET')}
                   </button>
@@ -198,21 +178,10 @@ function Report () {
                 </div>
               </div>
             </form>
-            <ReportsTable
-              users={users}
-              page={page}
-              pageSize={pageSize}
-              userType={userType}
-            />
+            <ReportsTable users={users} page={page} pageSize={pageSize} userType={userType}/>
             <div className='flex justify-between'>
             <PageSizeList  dynamicPage={dynamicPage} pageSize={pageSize}/>       
-              {paginationObj?.totalItems ? (
-                <Pagination
-                  handlePageClick={handlePageClick}
-                  options={paginationObj}
-                  page={page}
-                />
-              ) : null}
+              {paginationObj?.totalItems ? ( <Pagination handlePageClick={handlePageClick} options={paginationObj} page={page}/>) : null}
             </div>
           </div>
         </div>
