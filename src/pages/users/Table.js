d16b0d40e07d46e39f99fbd26ff02668a7950409
neha verma <neha.verma@octalsoftware.com>
2024-01-24 11:40:33 +0530

    solve sonar issues

--- src/pages/users/Table.js
+++ src/pages/users/Table.js
@@ -116,6 +116,75 @@ const Table = ({
   const renderTableCell = (content, classNames) => (
     <td className={classNames}>{content}</td>
   );
+  const renderCommonTableCells = (item) => (
+    <>
+      {renderTableCell(
+        getKycStatusText(item),
+        'py-4 px-3 border-r  dark:border-[#ffffff38] text-center'
+      )}
+      {renderTableCell(
+        dayjs(item?.createdAt).format('DD-MM-YYYY hh:mm A') || 'N/A',
+        'py-4 px-3 border-r  dark:border-[#ffffff38] text-center'
+      )}
+    </>
+  );
+  const renderActionTableCells = (item, userType) => (
+     <td className="py-2 px-4 border-l">
+          <div className="">
+            <div className="flex justify-center items-center">
+              <NavLink
+                onClick={() => handleUserView(item)}
+                to="/users/view"
+                state={{ ...item, userType }}
+                title={t("O_VIEW")}
+                className="px-2 py-2"
+              >
+                <AiFillEye className="cursor-pointer w-5 h-5 text-slate-600 dark:hover:text-white hover:text-blue-700" />{" "}
+              </NavLink>
+
+              {(manager?.add || user?.role === "admin") &&
+                userType === "local" &&
+                (item?.status !== "deleted" ? (
+                  <div onClick={() => handelEdit(item)}>
+                    <AiFillEdit
+                      className="text-green text-lg cursor-pointer  text-slate-600"
+                      title="Edit user"
+                    />
+                  </div>
+                ) : (
+                  ""
+                ))}
+              {(manager?.add || user?.role === "admin") &&
+                (item?.status !== "deleted" ? (
+                  <div
+                    onClick={() => {
+                      setIsAmountModal(true);
+                      setAddAmountUser(item);
+                    }}
+                  >
+                    <GiTakeMyMoney
+                      className="text-green text-lg cursor-pointer  text-slate-600"
+                      title="Add amount"
+                    />
+                  </div>
+                ) : (
+                  ""
+                ))}
+              <div>
+                <NavLink
+                  to="/users/transactionDetails"
+                  state={{ userType, userId: item?._id }}
+                >
+                  <MdHistory
+                    className="text-green text-lg cursor-pointer  text-slate-600"
+                    title="Transaction details"
+                  />
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </td> 
+  );
 
   const renderUserTypeSpecificCells = (item, userTypeData) => {
     if (userTypeData === 'local') {
@@ -225,76 +294,16 @@ const Table = ({
             : "text-slate-900"
         }`
       )}
+
       {renderUserTypeSpecificCells(item, userType)}
-          {renderTableCell(
-        getKycStatusText(item),
-        'py-4 px-3 border-r  dark:border-[#ffffff38] text-center'
-      )}
-         {renderTableCell(
-        dayjs(item?.createdAt).format('DD-MM-YYYY hh:mm A') || 'N/A',
-        'py-4 px-3 border-r  dark:border-[#ffffff38] text-center'
-      )}
+       {renderCommonTableCells(item)}
         {helpers.andOperator(
           manager?.add || user?.permission?.length === 0,
           <td className="py-2 px-4 border-r  dark:border-[#ffffff38] text-center">
             {statusLabel(item)}
           </td>
         )}
-        <td className="py-2 px-4 border-l">
-          <div className="">
-            <div className="flex justify-center items-center">
-              <NavLink
-                onClick={() => handleUserView(item)}
-                to="/users/view"
-                state={{ ...item, userType }}
-                title={t("O_VIEW")}
-                className="px-2 py-2"
-              >
-                <AiFillEye className="cursor-pointer w-5 h-5 text-slate-600 dark:hover:text-white hover:text-blue-700" />{" "}
-              </NavLink>
-
-              {(manager?.add || user?.role === "admin") &&
-                userType === "local" &&
-                (item?.status !== "deleted" ? (
-                  <div onClick={() => handelEdit(item)}>
-                    <AiFillEdit
-                      className="text-green text-lg cursor-pointer  text-slate-600"
-                      title="Edit user"
-                    />
-                  </div>
-                ) : (
-                  ""
-                ))}
-              {(manager?.add || user?.role === "admin") &&
-                (item?.status !== "deleted" ? (
-                  <div
-                    onClick={() => {
-                      setIsAmountModal(true);
-                      setAddAmountUser(item);
-                    }}
-                  >
-                    <GiTakeMyMoney
-                      className="text-green text-lg cursor-pointer  text-slate-600"
-                      title="Add amount"
-                    />
-                  </div>
-                ) : (
-                  ""
-                ))}
-              <div>
-                <NavLink
-                  to="/users/transactionDetails"
-                  state={{ userType, userId: item?._id }}
-                >
-                  <MdHistory
-                    className="text-green text-lg cursor-pointer  text-slate-600"
-                    title="Transaction details"
-                  />
-                </NavLink>
-              </div>
-            </div>
-          </div>
-        </td>
+        {renderActionTableCells(item, userType)}
       </tr>
     ))
   }
