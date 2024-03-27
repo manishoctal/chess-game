7ef2ed3fdbaccc31b6b0dae2abfa27724bfdb538
Manish Prajapati <manish.prajapati@octalsoftware.com>
2024-03-27 19:03:03 +0530

    zindex increased

--- src/layout/Loader.js
+++ src/layout/Loader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const Loader = () => {
   return (
-    <div id='loader' className='flex justify-center items-center h-screen fixed w-full z-10 hidden'>
+    <div id='loader' className='flex justify-center items-center h-screen fixed w-full z-100 hidden'>
       <svg className='animate-spin h-5 w-5 text-gray-600 relative' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
         <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
         <path
