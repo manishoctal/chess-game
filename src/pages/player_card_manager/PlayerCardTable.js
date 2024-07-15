1b66bbec1d6c73de258ff8ddfb64ba4b3d0c6a80
Abhishek Maurya <abhishekm@octallocal.com>
2024-07-15 18:54:02 +0530

    player card

--- src/pages/player_card_manager/PlayerCardTable.js
+++ src/pages/player_card_manager/PlayerCardTable.js
@@ -73,15 +73,11 @@ const PlayerCardTable = ({
                                 {getTableDataPlayerCard(helpers.getDateAndTime(item?.createdAt))}
                                 {getTableDataPlayerCard(helpers.getDateAndTime(item?.updatedAt))}
                                 {getTableDataPlayerCard(helpers.getDateAndTime(item?.updatedAt))}
-
                                 {getTableDataPlayerCard(helpers.getDateAndTime(item?.updatedAt))}
-
-
                                 <td className="py-2 px-4 border-l">
                                     <div className="">
                                         <ul className="flex justify-center">
-
-                                            {(manager?.add || user?.role === "admin") && (
+                                            {helpers.andOperator((helpers?.orOperator(manager?.add , user?.role === "admin")) , (
                                                 <li
                                                     onClick={() => { editCardLimit(item) }}
                                                     className="px-2 py-2 hover:text-gradientTo" >
@@ -89,12 +85,12 @@ const PlayerCardTable = ({
                                                         <AiFillEdit className="cursor-pointer w-5 h-5 text-slate-600" />
                                                     </a>
                                                 </li>
-                                            )}
-                                            {(manager?.view || user?.role === "admin") && (<li className="px-2 py-2 hover:text-gradientTo">
+                                            ))}
+                                            {helpers.andOperator((helpers?.orOperator(manager?.view , user?.role === "admin")), (<li className="px-2 py-2 hover:text-gradientTo">
                                                 <NavLink to='/' title={t("O_VIEW")} state={item}>
                                                     <AiFillEye className="cursor-pointer w-5 h-5 text-slate-600" />
                                                 </NavLink>
-                                            </li>)}
+                                            </li>))}
 
                                         </ul>
                                     </div>
