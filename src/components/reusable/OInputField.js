b08bbec813d2c3c2bff2e67e39a6c326ed005918
Abhishek Maurya <abhishekm@octallocal.com>
2024-07-26 15:16:07 +0530

    visibility in questuins

--- src/components/reusable/OInputField.js
+++ src/components/reusable/OInputField.js
@@ -1,5 +1,6 @@
 import ErrorMessage from 'components/ErrorMessage'
 import React from 'react'
+import helpers from 'utils/helpers'
 
 const OInputField = props => {
   const {
@@ -89,10 +90,10 @@ const OInputField = props => {
     case 'select':
       return (
         <>
-          <div className={wrapperClassName || 'relative z-0 mb-6 w-full group'}>
+          <div className={helpers.orOperator(wrapperClassName , 'relative z-0 mb-6 w-full group')}>
             <select
               id={name}
-              className={`${inputClass || 'block py-4 px-3 w-full text-sm text-gray-900 bg-transparent border-2 rounded-lg border-[#DFDFDF] appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer'}`}
+              className={`${helpers.orOperator(inputClass, 'block py-4 px-3 w-full text-sm text-gray-900 bg-transparent border-2 rounded-lg border-[#DFDFDF] appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0  peer')}`}
               placeholder=' '
               {...register}
               {...rest}
@@ -100,14 +101,14 @@ const OInputField = props => {
             >
               {selectOptions?.map(item => item)}
             </select>
-            {inputLabel &&
+            {helpers.andOperator(inputLabel,
               <label
                 htmlFor={name}
                 className='peer-focus:font-normal absolute text-sm text-[#A5A5A5] dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-3 bg-white p-2 z-10 origin-[2] peer-focus:left-0 peer-focus:text-[#A5A5A5] peer-focus:text-lg peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8'
               >
                 {inputLabel}
                 {labelType && <span className='text-red-500'>*</span>}
-              </label>}
+              </label>)}
           </div>
 
           <ErrorMessage message={errors?.[name]?.message} />
